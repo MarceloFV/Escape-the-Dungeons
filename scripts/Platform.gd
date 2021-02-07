3cfd93f7399eeee9cdd5c34422d8a2493d1ea5b1
Marcelo Fernandes <marcelofv12@hotmail.com>
2021-02-07 13:22:55 -0300

    commit

--- scripts/Platform.gd
+++ scripts/Platform.gd
@@ -4,7 +4,6 @@ extends StaticBody2D
 func _ready():
 	$CollisionShape2D.shape.extents = $Sprite.texture.get_size()/2
 	
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
